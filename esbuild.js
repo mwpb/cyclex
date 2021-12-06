f1cd92d8308ebc8d69981a8e7b92050746d62992
Matthew Burke <matthewpatrickburke@gmail.com>
2021-12-06 16:28:17 -0700

    Test netlify id

--- esbuild.js
+++ esbuild.js
@@ -1,7 +1,7 @@
 let isDev = false;
 if (process.argv.includes("dev")) isDev = true;
 
-require("esbuild").buildSync({
+require("esbuild").build({
   entryPoints: ["frontend/app.ts"],
   outfile: "dist/bundle/app.js",
   bundle: true,
--- frontend/app.ts
+++ frontend/app.ts
@@ -1,1 +1,14 @@
 console.log("Hi from netlify");
+
+// import * as netlifyIdentity from "netlify-identity-widget";
+
+// console.log(netlifyIdentity);
+
+// netlifyIdentity.on("login", user => {
+//   console.log(user);
+  
+// })
+
+// netlifyIdentity.init();
+// netlifyIdentity.open()
+
