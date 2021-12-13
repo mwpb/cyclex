68b66889e7d77aefdf2a36c9fb3c60345566ea1c
Matthew Burke <matthewpatrickburke@gmail.com>
2021-12-13 09:51:10 -0700

    Remove webpack bundle analyzer from config

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const webpack = require("webpack");
-const BundleAnalyzerPlugin =
-  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+// const BundleAnalyzerPlugin =
+//   require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
   experiments: {
